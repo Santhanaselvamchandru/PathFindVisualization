3348826b46df066e2ec6d937c70eb4c1369e2d8a
Santhana selvam chandru <santhanaselvamchandruv@gmail.com>
2021-02-25 13:07:17 +0530

    drag and drop added

--- PathFind/js/main.js
+++ PathFind/js/main.js
@@ -10,6 +10,8 @@ class UI{
             for(let j = 0 ; j<column;j++){
                 var td = document.createElement('td');
                 td.setAttribute('id',`${i+1}-${j+1}`);
+                td.setAttribute('ondrop','UI.onDrop(event)');
+                td.setAttribute('ondragover','UI.onDragover(event)');
                 tr.appendChild(td);
             }
             board.appendChild(tr);
@@ -17,12 +19,8 @@ class UI{
     }
     static wallCreation(row , col){
         let cells = board.querySelectorAll('td');
-        let get_row = row / 2;
-        let get_col = Math.round(col / 3);
-        let startPoint = document.getElementById(`${get_row}-${get_col}`); 
-        let targetPoint = document.getElementById(`${get_row}-${get_col + 15}`);
-        startPoint.appendChild(UI.nodeCreate('start'));
-        targetPoint.appendChild(UI.nodeCreate('target'));
+        this.startNode(row,col);
+        this.targetNode(row,col);
         cells.forEach((cell)=>{ 
 
                    
@@ -30,9 +28,39 @@ class UI{
     }
     static nodeCreate(name){
         var div = document.createElement('div');
-        div.setAttribute('class',name);
+        div.setAttribute('id',name);
         div.setAttribute('draggable','true');
+        div.setAttribute('ondragstart','UI.dragStart(event)')
         return div;
+    }
+    static startNode(row,col){
+        let get_row = row / 2;
+        let get_col = Math.round(col / 3);
+        let startPoint = document.getElementById(`${get_row}-${get_col}`); 
+        startPoint.appendChild(UI.nodeCreate('start'));
+        
+    }
+    static dragStart(e){
+        e.dataTransfer.setData('text/plain',e.target.id);
+    }
+    static onDrop(e){
+        e.preventDefault();
+        var data = e.dataTransfer.getData('text');
+        console.log(document.getElementById(data));
+        if(data != document.getElementById('start').parentElement.id){
+            e.target.appendChild(document.getElementById(data));
+        }
+        
+    }
+    //drag over event
+    static onDragover(e){
+        e.preventDefault();
+    }
+    static targetNode(row,col){
+        let get_row = row / 2;
+        let get_col = Math.round(col / 3); 
+        let targetPoint = document.getElementById(`${get_row}-${get_col + 15}`);
+        targetPoint.appendChild(UI.nodeCreate('target'));
     }
 }
 //Function calls 1280 521
