9fca7f487b38471da50f7e333bc0b6c88af9a2b2
Santhana selvam chandru <santhanaselvamchandruv@gmail.com>
2021-04-05 17:18:23 +0530

    modified

--- PathFind/js/main.js
+++ PathFind/js/main.js
@@ -1,125 +1,124 @@
-container = document.getElementById('container');
-let row = 10;
-let col = 20;
-let cells = [];
-let unvisited = undefined;
-//cell initialization
-class cell{
-    constructor(i,j,visit,wall){
-        this.row = i;
-        this.col = j;
-        this.isVisited = visit;
-        this.isWall = wall;
-    }
+let board = document.getElementById('board');
+let row = 15;
+let col = 30;
+let grid = [];
+let unvisited = [];
+
+// each column objects
+function Cell(i,j){
+    this.i = i;
+    this.j = j;
+    this.isVisited = false;
+    this.isWall = false;
 }
+// create cells in table
+function draw(r){
+    let tr = document.createElement('tr');
+    r.forEach(c =>{
+        let td = document.createElement('td');
+        td.id = `${c.i}-${c.j}`
+        if(c.isVisited){
+            td.setAttribute('class','visited');
+        }
+        if(c.isWall){
+            td.setAttribute('class','wall');
+        }
+        tr.appendChild(td);
+    })
+
+    board.appendChild(tr);
+}
+// push row and column objects in grid array
 function setup(){
     for(let i = 0;i<row;i++){
-        let rows = [];
+        let r = [];
         for(let j = 0;j<col;j++){
-            let cell_s = new cell(i,j,false,false);
-            rows.push(cell_s);
+            let cell = new Cell(i,j);
+            r.push(cell);
         }
-        cells.push(rows);
+        grid.push(r)
     }
-    //console.log(cells)
-    cells.forEach(r =>{
-        let tr = document.createElement('tr');
-        r.forEach( c =>{
-            let td = document.createElement('td');
-    
-            td.setAttribute('id',`${c.row}-${c.col}`);
-            tr.appendChild(td);
-
-            if(c.isVisited){
-                td.className += ' visited';
-            }
-            if(c.isWall){
-                td.className += ' wall';
-            }
-        })
-        container.appendChild(tr);
+    grid.forEach( g_r =>{
+        draw(g_r)
     })
 }
-function DFS(start_row,start_col,Queue){
-    let i = start_row;
-    let j = start_col;
-    //let target = '10-20';
-    let visit = document.getElementById(`${i}-${j}`);
-    visit.setAttribute('class','visited');
-    let left = document.getElementById(`${i}-${j-1}`);
-    let right = document.getElementById(`${i}-${j+1}`);
-    let top = document.getElementById(`${i-1}-${j}`);
-    let bottom = document.getElementById(`${i+1}-${j}`);
-    if(left){
-        if(left.className !== 'visited'){
-            Queue.push(left.id)              
-        }
-    }
-    if(right){
-        if(right.className !== 'visited'){
-            Queue.push(right.id)                   
-        }
-    }
-    if(top){
-        if(top.className !== 'visited'){
-            Queue.push(top.id)              
-        }
-    }
-    if(bottom){
-        if(bottom.className !== 'visited'){
-            Queue.push(bottom.id)                    
-        }
-    }
-    if(!Queue.isEmpty()){
-        let front = Queue.front;
-        let rear = Queue.rear;
-        for(let i = front;i<rear;i++){
-            let temp = Queue.pop();
-            temp = temp.split('-');
-            console.log(Queue.arr);
-            DFS(temp[0],temp[1],Queue);
 
-        }
+// display visited nodes
+function update_node(node){
+    let ele = document.getElementById(`${node.i}-${node.j}`);
+    if(node.isVisited){
+        ele.setAttribute('class','visited');
+    }
+    if(node.isWall){
+        ele.setAttribute('class','wall');
     }
 }
-class queue{
-    constructor(){
-        this.arr = [];
-        this.front = -1;
-        this.rear = -1;
-    }
-    push(item){
-        if(this.front == -1){
-            this.front = 0;
-            this.rear = 0;
-            this.arr[this.front] = item;
-            return;
+
+//sleep method
+const sleep = (ms) =>{
+    return new Promise(resolve => setTimeout(resolve,ms));
+}
+
+// Breadth First Search
+function BFS(start_node,target_node){
+    const dosearch = async()=>{
+        let s = document.getElementById(`${start_node.i}-${start_node.j}`);
+        let t = document.getElementById(`${target_node.i}-${target_node.j}`);
+        s.style.backgroundColor = 'green';
+        t.style.backgroundColor = 'red';
+        unvisited.push(start_node);
+
+        while(unvisited.length != 0 ){
+            await sleep(100);
+            console.log(unvisited.length);
+            let current_node = unvisited.shift();
+            if(current_node){
+                let c = document.getElementById(`${current_node.i}-${current_node.j}`);
+                if(current_node == target_node){
+                    break;
+                }
+                if(current_node.isVisited == false){
+                    current_node.isVisited = true;
+                    update_node(current_node);
+                    findNeighbours(current_node);    
+                }
+            }
         }
-        this.rear +=1;
-        this.arr[this.rear] = item;
     }
-    pop(){
-        if(this.rear == -1){
-            return false;
-        }
-        let temp = this.arr[this.front];
-        this.front += 1;
-        if(this.front >= this.rear){
-            this.front = -1;
-            this.rear = -1;
-        }
-        return temp;
+
+    dosearch();
+    
+}
+// Find neighbors on current node
+function findNeighbours(node){
+    if(node.i < grid.length && node.j-1 >= 0){
+        let left = grid[node.i][node.j-1];
+        let temp = unvisited.indexOf(left)
+        if(temp == -1)
+            unvisited.push(left);
     }
-    isEmpty(){
-        return (this.arr.length == 0)
+    if(node.i < grid.length && node.j+1 <= grid.length){
+        let right = grid[node.i][node.j+1];
+        let temp = unvisited.indexOf(right)
+        if(temp == -1)
+            unvisited.push(right);
+    }
+    if(node.i-1 >= 0 && node.j < grid.length){
+        let top = grid[node.i-1][node.j];
+        let temp = unvisited.indexOf(top);
+        if(temp == -1)
+            unvisited.push(top);
+    }
+    if(node.i+1 < grid.length && node.j >= 0){
+        let bottom = grid[node.i+1][node.j]
+        let temp = unvisited.indexOf(bottom);
+        if(temp == -1)
+            unvisited.push(bottom);
     }
 }
 setup();
-DFS(1,1,unvisited = new queue());
 
-
-
-
+BFS(grid[0][0],grid[10][9])
 
 
 
